3d9e461e40f1a0cccfe8a0cb768879eb2421dff8
Ludvig Kirkeby <luki@itu.dk>
2025-08-29 09:52:17 +0200

    Initial push

---
+++ Chirp.CLI/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<string> cheeps = new() { "Hello everynyan!", "Welcome to sloppity slop course!", "I miss summer." };
+
+foreach (var cheep in cheeps) {
+    Console.WriteLine(cheep);
+    Thread.Sleep(1000);
+}

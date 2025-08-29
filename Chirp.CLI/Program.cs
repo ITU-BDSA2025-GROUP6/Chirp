f09bbea76a863cbcae67e4845deb0fe762803cfd
Ludvig Kirkeby <luki@itu.dk>
2025-08-29 12:06:11 +0200

    Parse and output csv data
    Co-Author ludvig.elias.kirkeby@gmail.com, cenh@itu.dk, anys@itu.dk, sebseb10@gmail.com, mcls@itu.dk

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,8 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Globalization;
 
-List<string> cheeps = new() { "Hello everynyan!", "Welcome to sloppity slop course!", "I miss summer." };
-
-foreach (var cheep in cheeps) {
-    Console.WriteLine(cheep);
-    Thread.Sleep(1000);
+try
+{
+    string filePath = System.IO.Path.GetFullPath("Data/chirp_cli_db.csv");
+    using(var parser = new TextFieldParser(filePath))
+    {
+        parser.SetDelimiters(",");                     
+        parser.HasFieldsEnclosedInQuotes = true;  
+        
+        // remove first line
+        String[] header = parser.ReadFields();
+        
+        while (!parser.EndOfData)
+        {
+            var line = parser.ReadFields();
+            String author = line[0];
+            String message = line[1];
+            var date  = DateTimeOffset.FromUnixTimeSeconds(long.Parse(line[2])).DateTime;
+            
+            Console.WriteLine(author + " @ " + date + ": " + message);
+        }
+    }
+    // for writing to the csv
+    //using (var writer = new StreamWriter(filePath))
+    //{
+    //    writer.WriteLine("Test");
+    //}
+    
+} catch (Exception e)
+{
+    Console.WriteLine(e.Message);
 }
+
+

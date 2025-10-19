541b4369f783295f71418725783a036678cca06c
mcls <mcls@itu.dk>
2025-10-19 21:06:58 +0200

    add dispose method from IDisposable to CheepRepositoryTests.cs

--- test/Database_Test/CheepRepositoryTests.cs
+++ test/Database_Test/CheepRepositoryTests.cs
@@ -1,6 +1,30 @@
+using Chirp.Core;
+using Chirp.Infrastructure;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
 namespace Database_Test;
 
-public class CheepRepositoryTests
+public class CheepRepository_Tests : IDisposable
 {
+
+    private readonly SqliteConnection _connection;
+    private readonly CheepRepository _repository;
+    private readonly CheepDBContext _context;
+
+    public CheepRepository_Tests()
+    {
+        _connection = new SqliteConnection("Data Source=:memory:");
+        _connection.Open();
+    }
     
+    public void Dispose()
+    {
+        // this method is called after all tests in the class have run
+        // disposes of connection
+        _connection.Close();
+        _connection.Dispose();
+        _context.Dispose();
+    }
 }

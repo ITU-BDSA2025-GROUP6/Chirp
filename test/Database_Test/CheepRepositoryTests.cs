755dbbfa8bf9e380e9534ee93483e3e3f440eca6
mcls <mcls@itu.dk>
2025-10-20 11:54:30 +0200

    Write test method for createCheep from cheepRepository to see if it adds cheep to DB

--- test/Database_Test/CheepRepositoryTests.cs
+++ test/Database_Test/CheepRepositoryTests.cs
@@ -24,6 +24,33 @@ public class CheepRepository_Tests : IDisposable
         
         _context = new CheepDBContext(options);
         _context.Database.EnsureCreated();
+        
+        var author = new Author { AuthorID = 1, Name = "Test Author", Email = "test@author.com", Cheeps = new List<Cheep>() };
+        _context.Authors.Add(author);
+        _context.SaveChanges();
+        
+        _repository = new CheepRepository(_context);
+    }
+
+    [Fact]
+    public async Task createCheep_ShouldAddCheepToDatabase()
+    {
+        //Arrange
+        var newCheep = new CheepDTO
+        {
+            AuthorName = "Test Author",
+            Text = "Hello, this is a test cheep!",
+            Timestamp = DateTime.UtcNow
+        };
+
+        //Act
+        await _repository.CreateCheep(newCheep);
+        var createdCheep = await _context.Cheeps.Include(c => c.Author).FirstOrDefaultAsync();
+        //Assert
+        Assert.NotNull(createdCheep);
+        Assert.Equal(1,  createdCheep.CheepID);
+        Assert.Equal("Test Author", createdCheep.Author.Name);
+        Assert.Equal("Hello, this is a test cheep!", createdCheep.Text);
     }
     
     public void Dispose()

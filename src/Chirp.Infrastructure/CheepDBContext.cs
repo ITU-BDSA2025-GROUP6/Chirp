31bfb89ada1a6504d88ef8152b181e570d866993
mcls <mcls@itu.dk>
2025-10-30 20:15:58 +0100

    make CheepDBContext.cs extend IdentityDbContext Co-authored-by: Sebastian Holt Svendsen <sesv@itu.dk>

--- src/Chirp.Infrastructure/CheepDBContext.cs
+++ src/Chirp.Infrastructure/CheepDBContext.cs
@@ -1,10 +1,13 @@
 using Chirp.Core;
+using Chirp.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace Chirp.Infrastructure;
 
 
-public class CheepDBContext : DbContext
+public class CheepDBContext : IdentityDbContext<ApplicationUser>
 {
     public DbSet<Cheep> Cheeps { get; set; }
     public DbSet<Author> Authors { get; set; }

72c53c3a68a53662e44b6b19afc5b7745511c5ed
cenh <cenh@itu.dk>
2025-10-30 19:08:31 +0100

    include Cheeps when getting author by email

--- src/Chirp.Infrastructure/CheepRepository.cs
+++ src/Chirp.Infrastructure/CheepRepository.cs
@@ -80,6 +80,7 @@ public class CheepRepository : ICheepRepository
         try
         {
             var author = await _dbContext.Authors
+                .Include(a => a.Cheeps)
                 .FirstAsync(a => a.Email == email);
 
             return new AuthorDTO

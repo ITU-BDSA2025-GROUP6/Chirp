a53597bf6aa7f2dc1e37e75902c10a1ce4f625fa
cenh <cenh@itu.dk>
2025-10-31 12:44:56 +0100

    fix Non-nullable property warnings

--- src/Chirp.Infrastructure/AuthorDTO.cs
+++ src/Chirp.Infrastructure/AuthorDTO.cs
@@ -3,12 +3,12 @@ using Chirp.Core;
 namespace Chirp.Infrastructure;
 
 public class AuthorDTO
-{
+{ 
     public int AuthorID  { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
     
-    public string Name { get; set; }
-    
-    public string Email { get; set; }
-    
-    public ICollection<Cheep> Cheeps { get; set; }
+    public ICollection<Cheep> Cheeps { get; set; }  = new List<Cheep>();
 }

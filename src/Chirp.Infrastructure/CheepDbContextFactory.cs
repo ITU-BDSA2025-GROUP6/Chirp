0c0f12c4ac6dc81cd51ba5f2684614e27404ffa8
mcls <mcls@itu.dk>
2025-11-03 22:25:25 +0100

    Make the DbContextFactory
    Co-authored-by: Sebastian <sebseb10@gmail.com>

--- src/Chirp.Infrastructure/CheepDbContextFactory.cs
+++ src/Chirp.Infrastructure/CheepDbContextFactory.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
 namespace Chirp.Infrastructure;
 
-public class CheepDbContextFactory
+public class CheepDbContextFactory : IDesignTimeDbContextFactory<CheepDBContext>
 {
-    
+    public CheepDBContext CreateDbContext(string[] args)
+    {
+        var options = new DbContextOptionsBuilder<CheepDBContext>()
+            .UseSqlite("Data Source=Chirp.db")
+            .Options;
+        
+        return new CheepDBContext(options);
+    }
 }

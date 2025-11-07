a62388ae187b8745837d15dae4df44b719c5818b
Ludvig Kirkeby <luki@itu.dk>
2025-11-07 12:13:41 +0100

    Changed root namespace from razor to web

--- src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -18,7 +18,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
-namespace Chirp.Razor.Areas.Identity.Pages.Account
+namespace Chirp.Web.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class ExternalLoginModel : PageModel
--- src/Chirp.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace Chirp.Razor.Areas.Identity.Pages.Account
+namespace Chirp.Web.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {

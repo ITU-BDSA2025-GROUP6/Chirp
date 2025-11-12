04be8d38877667fd2a1fce5302dcd6674abea48f
Sebastian Holt Svendsen <sesv@itu.dk>
2025-11-12 15:40:51 +0100

    Change to handle authorId being a string
    Co-authored-by: Maja <mcls@itu.dk>
    Co-authored-by: Nina Celine Hansson <cenh@itu.dk>
    Co-authored-by: Ludvig Kirkeby <luki@itu.dk>
    Co-authored-by: Andreas <anys@itu.dk>

--- src/Chirp.Core/Cheep.cs
+++ src/Chirp.Core/Cheep.cs
@@ -16,9 +16,7 @@ namespace Chirp.Core
         [Required]
         public DateTime Timestamp { get; set; }
 
-        //Unsure if this is required anymore
-        [Required]
-        public required Author Author { get; set; }
+        public Author Author { get; set; }
         
         [ForeignKey(nameof(Author))]
         public int AuthorID { get; set; }

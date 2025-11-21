5e151960d87ab4f3977a373bd90f28efc96a8c5d
annakbh <anna.kirstine.hoff@gmail.com>
2025-11-21 11:27:45 +0100

    follow buttons added to timeline and usertimeline

--- src/Chirp.Infrastructure/AuthorDTO.cs
+++ src/Chirp.Infrastructure/AuthorDTO.cs
@@ -11,4 +11,8 @@ public class AuthorDTO
     public string Email { get; set; }
     
     public ICollection<Cheep> Cheeps { get; set; }
+    
+    public List<AuthorDTO> Followers { get; set; }
+    
+    public List<AuthorDTO> Following { get; set; }
 }

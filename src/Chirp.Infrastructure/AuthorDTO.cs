b458d7b3cc2b5080bebd0c68b6b61539a5a9a418
Ludvig Kirkeby <luki@itu.dk>
2025-11-28 14:30:07 +0100

    Added Recheeps under Public.cshtml - But it fails right now

--- src/Chirp.Infrastructure/AuthorDTO.cs
+++ src/Chirp.Infrastructure/AuthorDTO.cs
@@ -11,4 +11,6 @@ public class AuthorDTO
     public string Email { get; set; } = string.Empty;
     
     public ICollection<Cheep> Cheeps { get; set; } = new List<Cheep>();
+
+    public ICollection<int> RecheepIDs { get; set; } = new List<int>();
 }
--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -10,14 +10,16 @@ namespace Chirp.Web.Pages;
 public class PublicModel : PageModel
 {
     private readonly ICheepService _service;
+    private readonly IAuthorService _authservice;
     public Task<List<CheepDTO>> Cheeps { get; set; } = Task.FromResult(new List<CheepDTO>());
     
     [BindProperty]
     [StringLength(160, ErrorMessage = "The {0} must be at max {1} characters long.")]
     public string Text { get; set; } = string.Empty;
-    public PublicModel(ICheepService service)
+    public PublicModel(ICheepService service, IAuthorService authservice)
     {
         _service = service;
+        _authservice = authservice;
     }
 
     public ActionResult OnGet()
@@ -26,6 +28,26 @@ public class PublicModel : PageModel
         Cheeps = _service.GetCheeps(currentpage);
         return Page();
     }
+
+   public async Task<IActionResult> OnPostRecheepAsync(int CheepID)
+   {
+   var authorName = User.Identity?.Name;
+   var author = await _authservice.GetAuthorByName(authorName);
+
+   if (author == null) {
+    return Redirect("/");
+   }
+
+    // fallback
+   if (author.RecheepIDs == null) { author.RecheepIDs = new List<int>(); }
+
+   author.RecheepIDs.Add(CheepID);
+   foreach (var a in author.RecheepIDs) {
+   Console.WriteLine("Recheep IDs for Author: " + a);
+   }
+
+   return RedirectToPage();
+   }
 
     public IActionResult OnPost()
     {

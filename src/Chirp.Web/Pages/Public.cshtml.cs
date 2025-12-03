97e050bcfddaae6e88bd98182725fe105e2d8a95
Sebastian Holt Svendsen <sesv@itu.dk>
2025-12-03 21:57:21 +0100

    Change Public.cshtml.cs methods to be Async to handle multiple user requests at once. Make posting on Azure possible by keeping the Db awake.

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -11,7 +11,7 @@ public class PublicModel : PageModel
 {
     private readonly ICheepService _service;
     private readonly IAuthorService _authorService;
-    public Task<List<CheepDTO>> Cheeps { get; set; } = Task.FromResult(new List<CheepDTO>());
+    public List<CheepDTO> Cheeps { get; set; } = new List<CheepDTO>();
     
     public Task<List<Author>>? Followers { get; set; }
     
@@ -24,18 +24,18 @@ public class PublicModel : PageModel
         _authorService = authorService;
     }
 
-    public ActionResult OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
         int currentPage = 1;
-        Cheeps = _service.GetCheeps(currentPage);
+        Cheeps = await _service.GetCheeps(currentPage);
         return Page();
     }
 
-    public IActionResult OnPost()
+    public async Task<IActionResult> OnPostAsync()
     {
         if (!ModelState.IsValid)
         {
-            Cheeps = _service.GetCheeps(1);
+            Cheeps = await _service.GetCheeps(1);
             return Page();
         }
         if (!(User.Identity?.IsAuthenticated ?? false ))
@@ -53,13 +53,13 @@ public class PublicModel : PageModel
             Text = Text,
             Timestamp = DateTime.UtcNow
         };
-        _service.CreateCheep(newCheep);
+        await _service.CreateCheep(newCheep);
         
         
         return Redirect("/");
     }
 
-    public async Task<IActionResult> OnPostDelete(int cheepId)
+    public async Task<IActionResult> OnPostDeleteAsync(int cheepId)
     {
         if (! User.Identity?.IsAuthenticated ?? true)
         {
@@ -75,11 +75,8 @@ public class PublicModel : PageModel
     }
 
 
-    public async Task<IActionResult> OnGetFollowBtn(string authorName)
+    public async Task<IActionResult> OnGetFollowBtnAsync(string authorName)
     {
-        //if (!User.Identity.IsAuthenticated)
-           // return Redirect("/");
-           
         var username = User.Identity?.Name;
         if (string.IsNullOrEmpty(username)) return Redirect("/");
 

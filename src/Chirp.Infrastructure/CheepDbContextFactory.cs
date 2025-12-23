ba07bf76f3fb5a56923a69e988639997b811106f
mcls <mcls@itu.dk>
2025-12-23 17:58:41 +0100

    change Design Time Context factory to support SQL server and SQLite

--- src/Chirp.Infrastructure/CheepDbContextFactory.cs
+++ src/Chirp.Infrastructure/CheepDbContextFactory.cs
@@ -7,10 +7,42 @@ public class CheepDbContextFactory : IDesignTimeDbContextFactory<CheepDBContext>
 {
     public CheepDBContext CreateDbContext(string[] args)
     {
-        var options = new DbContextOptionsBuilder<CheepDBContext>()
-            .UseSqlite("Data Source=Chirp.db")
-            .Options;
-        
-        return new CheepDBContext(options);
+
+        var provider = "sqlite"; //default
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "--provider" && i + 1 < args.Length)
+            {
+                provider = args[i + 1].ToLower();
+                break;
+            }
+        }
+
+        var envProvider = Environment.GetEnvironmentVariable("EF_PROVIDER");
+        if (!string.IsNullOrEmpty(envProvider))
+        {
+            provider = envProvider.ToLower();
+        }
+
+        var optionsBuilder = new DbContextOptionsBuilder<CheepDBContext>();
+
+        if (provider == "sqlserver")
+        {
+            // For migration generation, we just need a valid connection string format
+            // The actual connection doesn't need to work
+            optionsBuilder.UseSqlServer(
+                "Server=. ;Database=Chirp;Trusted_Connection=True;",
+                x => x.MigrationsAssembly("Chirp.Infrastructure")
+                    .MigrationsHistoryTable("__EFMigrationsHistory", "dbo"));
+        }
+        else
+        {
+            optionsBuilder.UseSqlite(
+                "Data Source=Chirp.db",
+                x => x.MigrationsAssembly("Chirp.Infrastructure"));
+        }
+
+        return new CheepDBContext(optionsBuilder.Options);
     }
 }

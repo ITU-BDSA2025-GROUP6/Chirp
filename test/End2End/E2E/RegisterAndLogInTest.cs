6e4cd1c1f21ee9b411d0332b9d43d88e339589bf
cenh <cenh@itu.dk>
2025-12-28 17:21:12 +0100

    change to http instead of https

--- test/End2End/E2E/RegisterAndLogInTest.cs
+++ test/End2End/E2E/RegisterAndLogInTest.cs
@@ -46,7 +46,7 @@ namespace End2End
         {
             var page = await CreatePageAsync();
             
-            await page.GotoAsync("https://localhost:5273/");
+            await page.GotoAsync("http://localhost:5273/");
             await page.GetByRole(AriaRole.Link, new() { Name = "Register" }).ClickAsync();
             await page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).ClickAsync();
             await page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).FillAsync("testemail@example.com");
@@ -70,7 +70,7 @@ namespace End2End
         {
             var page = await CreatePageAsync();
             
-            await page.GotoAsync("https://localhost:5273/");
+            await page.GotoAsync("http://localhost:5273/");
             await page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
             await page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).ClickAsync();
             await page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).FillAsync("testemail@example.com");

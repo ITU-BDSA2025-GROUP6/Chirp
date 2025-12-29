364d250e344357180cb3b6f59663c7d1f507f98f
cenh <cenh@itu.dk>
2025-12-29 18:22:07 +0100

    remove unused test case

--- test/End2End/RegisterAndLogInTest.cs
+++ test/End2End/RegisterAndLogInTest.cs
@@ -85,41 +85,5 @@ namespace End2End
             await page.GetByRole(AriaRole.Button, new() { Name = "Delete data and close my" }).ClickAsync();
 
         }
-
-        /*
-        [Test]
-        public async Task RegisterAndLogIn2()
-        {
-            var context = await _browser.NewContextAsync();
-            var page = await context.NewPageAsync();
-
-            await page.GotoAsync("https://localhost:5273/");
-            /* 
-            User already registered !!! - CHECK issue #139 !!!
-            ¨
-            await page.GetByRole(AriaRole.Link, new() { Name = "Register" }).ClickAsync();
-            await page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).ClickAsync();
-            await page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).FillAsync("testemail@example.com");
-            await page.GetByRole(AriaRole.Textbox, new() { Name = "Password", Exact = true }).ClickAsync();
-            await page.GetByRole(AriaRole.Textbox, new() { Name = "Password", Exact = true }).FillAsync("Qwerty123!");
-            await page.GetByRole(AriaRole.Textbox, new() { Name = "Confirm Password" }).ClickAsync();
-            await page.GetByRole(AriaRole.Textbox, new() { Name = "Confirm Password" }).FillAsync("Qwerty123!");
-            await page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
-            await page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" }).ClickAsync();
-            
-            
-            await page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
-            await page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).ClickAsync();
-            await page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).FillAsync("testemail@example.com");
-            await page.GetByRole(AriaRole.Textbox, new() { Name = "Password" }).ClickAsync();
-            await page.GetByRole(AriaRole.Textbox, new() { Name = "Password" }).FillAsync("Qwerty123!");
-            await page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-            await page.Locator("#Text").ClickAsync();
-            await page.Locator("#Text").FillAsync("cheep");
-            await page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
-            await page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
-        }
-        */
     }
-
 }
